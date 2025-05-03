e9174217007d57668ba0d51db279a75686f58f86
Jorge Cifuentes Pérez <jorge.cifuentes.perez@gmail.com>
2025-05-03 13:18:06 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -33,14 +34,18 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : ""} ${
+    variables.lastName ? variables.lastName : ""
+  }</h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""} ${
+    variables.country ? variables.country : ""
+  }</h3>
+          <ul class=${variables.socialMediaPosition}> 
+            <li><a href="https://twitter.com/jorgecifu17"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/jorgecifu11"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/jorge-cifuentes-perez-428525176/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/jorgecifu11"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
